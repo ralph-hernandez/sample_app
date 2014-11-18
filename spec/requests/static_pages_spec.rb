0c915707df414dbf5bf08790155350b942622ebb
ralph-hernandez <ralph0511@yahoo.com>
2014-11-19 00:06:44 +0800

    Finish the static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+# require 'capybara/rspec'
+
+# RSpec.describe "StaticPages", :type => :request do
+#   describe "GET /static_pages" do
+#     it "works! (now write some real specs)" do
+#       get static_pages_index_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
+#  
+
+# require 'spec_helper'
+
+describe "Static pages" do
+
+	describe "Home page" do
+		
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Sample App')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Rails | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => "Rails | Help")
+		end
+	end
+
+	describe "About page" do	
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => "Rails | About Us")
+		end
+	end
+end
